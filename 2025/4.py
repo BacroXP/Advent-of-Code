08e6f09aaa97057ead05b37a9ec5093d2ae1e6fd
BacroXP <135527354+BacroXP@users.noreply.github.com>
2025-12-11 15:24:49 +0100

    Update 4.py
    DIrection handling

--- 2025/4.py
+++ 2025/4.py
@@ -13,11 +13,16 @@ def liftable():
         for y in range(len(inp[0])):
             i = 0
 
-            for dx, dy in [(-1, -1), (-1, 0), (1, 0), (0, -1), (0, 1), (1, 1), (1, -1), (-1, 1)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < len(inp) and 0 <= ny < len(inp[0]):
-                    if inp[nx][ny] == "@" or inp[nx][ny] == "x":
-                        i += 1
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    if dx == dy == 0:
+                        continue
+
+                    nx, ny = x + dx, y + dy
+
+                    if 0 <= nx < len(inp) and 0 <= ny < len(inp[0]):
+                        if inp[nx][ny] == "@" or inp[nx][ny] == "x":
+                            i += 1
         
             if i < 4 and inp[x][y] == "@":
                 inp[x][y] = "x"
