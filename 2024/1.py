a3c883c4f01282e23589e418849d2fa1b4f141a8
BacroXP <135527354+BacroXP@users.noreply.github.com>
2025-12-12 17:41:07 +0100

    Update 1.py

--- 2024/1.py
+++ 2024/1.py
@@ -1,39 +1,10 @@
-from collections import Counter
+sol1, sol2 = 0, 0
+file = open("input.txt").read().split()
+right = [int(n) for n in file[1::2]]
 
+for l, r in zip(sorted([int(n) for n in file[::2]]), sorted(right)):
+    sol1 += abs(l - r)
+    sol2 += l * right.count(l)
 
-def sol(address):
-    input_data = open(address).read().strip()
-    cmds = input_data.split('\n')
-    left_values = []
-    right_values = []
-    right_count = Counter()
-    
-    for cmd in cmds:
-        parts = cmd.split()
-        if len(parts) == 2:
-            left, right = int(parts[0]), int(parts[1])
-            left_values.append(left)
-            right_values.append(right)
-            right_count[right] += 1
-    
-    p1 = 0
-    left_values = sorted(left_values)
-    right_values = sorted(right_values)
-    
-    for l, r in zip(left_values, right_values):
-        p1 += abs(r - l)
-    
-    print("Solution 1: " + str(p1))
-    
-    p2 = 0
-    for l in left_values:
-        p2 += l * right_count.get(l, 0)
-    
-    print("Solution 2: " + str(p2))
-
-
-test_link = "test.txt"
-real_link = "input.txt"
-
-sol(test_link)
-sol(real_link)
+print(sol1)
+print(sol2)
